bfeeda11bd6189d0d1f700bce22361972061c629
JosephAyo <josephayo110@gmail.com>
2019-10-08 17:41:32 +0100

    first

---
+++ app.js
@@ -0,0 +1,50 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var fs = require('fs');
+var app = express();
+var socket = require('socket.io');
+const ip = require('ip');
+
+
+const port = process.env.port || 3000;
+
+var server= app.listen(port,'0.0.0.0', () => {
+    console.log(ip.address());
+    console.log(`app is listening on port: ${ip.address()}:${port}`);
+});
+
+
+// var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: false
+}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.static(path.join(__dirname)));
+// app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+
+var io=socket(server);
+io.on('connection',(socket)=>{
+    console.log('a user connected');
+    socket.on('disconnect',()=>{
+        console.log('a user disconnected');
+    });
+});
+
+setInterval(()=>{
+    io.emit('image','data');
+},1000);
+
+
+
+module.exports = app;
---
+++ public/javascripts/script.js
@@ -0,0 +1,53 @@
+const socket = io.connect('https://desolate-retreat-23275.herokuapp.com/') || io.connect('http://192.168.8.113:3000/');
+var videoElement = document.getElementById('video');
+var canvasElement = document.getElementById('canvas');
+var context = canvasElement.getContext('2d');
+const fps = 90;
+
+
+
+if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+    navigator.mediaDevices.getUserMedia({
+        video: true,
+        audio: true
+    }).then(function (stream) {
+        //video.src = window.URL.createObjectURL(stream);
+        videoElement.srcObject = stream;
+        videoElement.play();
+    });
+} else if (navigator.getUserMedia) { // Standard
+    navigator.getUserMedia({
+        video: true,
+        audio: true
+    }, function (stream) {
+        videoElement.src = stream;
+        videoElement.play();
+    }, errBack);
+} else if (navigator.webkitGetUserMedia) { // WebKit-prefixed
+    navigator.webkitGetUserMedia({
+        video: true,
+        audio: true
+    }, function (stream) {
+        videoElement.src = window.webkitURL.createObjectURL(stream);
+        videoElement.play();
+    }, errBack);
+} else if (navigator.mozGetUserMedia) { // Mozilla-prefixed
+    navigator.mozGetUserMedia({
+        video: true,
+        audio: true
+    }, function (stream) {
+        videoElement.srcObject = stream;
+        videoElement.play();
+    }, errBack);
+}
+
+
+
+setInterval(() => {
+    context.drawImage(videoElement, 0, 0, 640, 480);
+}, 1000 / fps);
+
+
+socket.on('image', (data) => {
+    console.log(data);
+});
