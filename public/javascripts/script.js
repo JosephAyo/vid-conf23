e1add62b6f07257255a26d4d005ae4e8f22e3696
JosephAyo <josephayo110@gmail.com>
2019-10-15 14:56:19 +0100

    refactored code to ES6

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,4 +1,4 @@
-const socket =  io.connect('https://vid-conf23.herokuapp.com/');
+const socket = io.connect('https://vid-conf23.herokuapp.com/');
 //||io.connect('http://localhost:3000/')
 
 
@@ -13,9 +13,9 @@ new Vue({
         show: false
     },
     methods: {
-        allUsers:()=>{
+        allUsers: function () {
             console.log('hit get all users');
-            const vm = this;
+            var vm = this;
             fetch('/users', {
                 method: 'GET',
                 withCredentials: true,
@@ -32,7 +32,7 @@ new Vue({
                 console.log(`error: ${err}`);
             });
         },
-        fetchUser:()=> {
+        fetchUser: function () {
             this.show = true;
             console.log(this.Users[this.index].data._id);
             this.viewedUserGen = this.Users[this.index];
@@ -41,12 +41,12 @@ new Vue({
     }
 });
 
-const videoElement = document.getElementById('video');
-const videoElement2 = document.getElementById('video-2');
-const audioElement = document.getElementById('aud');
-const canvasElement = document.getElementById('canvas');
-const stopButton = document.getElementById('stopBtn');
-const context = canvasElement.getContext('2d');
+var videoElement = document.getElementById('video');
+var videoElement2 = document.getElementById('video-2');
+var audioElement = document.getElementById('aud');
+var canvasElement = document.getElementById('canvas');
+var stopButton = document.getElementById('stopBtn');
+var context = canvasElement.getContext('2d');
 
 //FRAMES PER SECOND (THIS WILL BE CHANGED TO A VARYING VALUE SO AS TO ALLOW
 // FOR SO FRAMES TO BE SKIPPED IN CASE OF POOR NETWORK CONNECTION)
@@ -57,131 +57,18 @@ const fps = 100;
 let supported = navigator.mediaDevices.getSupportedConstraints();
 
 
-
-
-//function to get id information of the needed devices
-const device = () => navigator.mediaDevices.enumerateDevices()
-    .then(res => {
-        const devices = res.map((element) => {
-            console.log(`element ${element.toJSON()}`);
-            return element;
-        });
-        console.log('from promise', devices);
-        return devices[0];
-        // return res[3].toJSON();
-    }).catch(err => {
-        console.log('error', err);
-    });
-
-
-// console.log('support', supported);
-// console.log('device',device);
-
-device().then(deviceInfo => {
-    console.log('result: ', deviceInfo);
-    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-        console.log(`first`);
-        navigator.mediaDevices.getUserMedia({
-            video: true,
-            audio: {
-                groupId: {
-                    exact: deviceInfo.groupId
-                },
-                kind: {
-                    exact: "audioinput"
-                }
-            }
-        }).then(MediaStream => {
-            //video.src = window.URL.createObjectURL(stream);
-            videoElement.srcObject = MediaStream;
-            window.stream = MediaStream;
-            console.log(`stream: ${MediaStream}`);
-            videoElement.play();
-        });
-    } else if (navigator.getUserMedia) { // Standard
-        console.log(`standard`);
-        navigator.getUserMedia({
-            video: true,
-            audio: {
-                groupId: {
-                    exact: deviceInfo.groupId
-                },
-                kind: {
-                    exact: "audioinput"
-                }
-            }
-        }, stream => {
-            videoElement.src = stream;
-            console.log(`stream: ${stream}`);
-            videoElement.play();
-        }, errBack);
-    } else if (navigator.webkitGetUserMedia) { // WebKit-prefixed
-        console.log(`webkit`);
-        navigator.webkitGetUserMedia({
-            video: true,
-            audio: {
-                groupId: {
-                    exact: deviceInfo.groupId
-                    // 'b2b92d582ab6037118adba78cfbda558c0f072f410923b87d16e042f75963a8c'
-                },
-                kind: {
-                    exact: "audioinput"
-                }
-            }
-        }, stream => {
-            videoElement.src = window.webkitURL.createObjectURL(stream);
-            console.log(`stream: ${stream}`);
-            videoElement.play();
-        }, errBack);
-    } else if (navigator.mozGetUserMedia) { // Mozilla-prefixed
-        console.log(`moz`);
-        navigator.mozGetUserMedia({
-            video: true,
-            audio: {
-                groupId: {
-                    exact: deviceInfo.groupId
-                    // 'b2b92d582ab6037118adba78cfbda558c0f072f410923b87d16e042f75963a8c'
-                },
-                kind: {
-                    exact: "audioinput"
-                }
-            }
-        }, stream => {
-            videoElement.srcObject = stream;
-            console.log(`stream: ${stream}`);
-            videoElement.play();
-        }, errBack);
-    }
-});
-
-stopButton.addEventListener('click', () => {
-    // Stop all video streams.
-    videoElement.srcObject.getVideoTracks().forEach(track => track.stop());
-});
-
-//
-setInterval(() => {
-    context.drawImage(videoElement, 0, 0, 640, 480);
-    // console.log(`vid src: ${videoElement}`);
-    const src = canvasElement.toDataURL("image/webp");
-    socket.emit('newDraw', src);
-}, 1000 / fps);
-
-
-
 //CALL FUNCTION TO CONNECT TWO CURRENTLY LOGGED IN USERS
 //IT BASICALLY CONTINOUSLY RENDER IN A IMAGE ELEMENT THE VIDEO FEED FROM A USER 
 //TO ANOTHERS' SCREEN
-const call= ()=>{
-    const caller = JSON.parse(window.localStorage.getItem('user'));
-    const called = JSON.parse(window.localStorage.getItem('calledUser'));
+function call() {
+    var caller = JSON.parse(window.localStorage.getItem('user'));
+    var called = JSON.parse(window.localStorage.getItem('calledUser'));
     if (caller == called) {
         alert("You can't call yourself");
     } else {
         console.log(`placing a call to ${called}`);
         socket.emit('dialling', called);
         socket.on('pickUp', dialled => {
-            prompt(`a call was placed to ${dialled}`);
             //allow stream if the dialled user coincides with the presently logged in user
             if (caller == dialled) {
                 socket.on('draw', (vidsrc) => {
@@ -203,7 +90,7 @@ const call= ()=>{
                                         exact: "audiooutput"
                                     }
                                 }
-                            }).then(MediaStream => {
+                            }).then(function (MediaStream) {
                                 //video.src = window.URL.createObjectURL(stream);
                                 audioElement.srcObject = MediaStream;
                                 window.stream = MediaStream;
@@ -222,7 +109,7 @@ const call= ()=>{
                                         exact: "audiooutput"
                                     }
                                 }
-                            }, MediaStream => {
+                            }, function (MediaStream) {
                                 audioElement.srcObject = MediaStream;
                                 window.stream = MediaStream;
                                 console.log(`stream: ${MediaStream}`);
@@ -240,7 +127,7 @@ const call= ()=>{
                                         exact: "audiooutput"
                                     }
                                 }
-                            }, MediaStream => {
+                            }, function (MediaStream) {
                                 audioElement.srcObject = MediaStream;
                                 window.stream = MediaStream;
                                 console.log(`stream: ${MediaStream}`);
@@ -258,7 +145,7 @@ const call= ()=>{
                                         exact: "audiooutput"
                                     }
                                 }
-                            }, MediaStream => {
+                            }, function (MediaStream) {
                                 audioElement.srcObject = MediaStream;
                                 window.stream = MediaStream;
                                 console.log(`stream: ${MediaStream}`);
@@ -273,6 +160,120 @@ const call= ()=>{
         });
     }
 }
+
+//function to get id information of the needed devices
+var device = function () {
+    return navigator.mediaDevices.enumerateDevices()
+        .then(res => {
+            var devices = res.map((element) => {
+                console.log(`element ${element.toJSON()}`);
+                return element;
+            });
+            console.log('from promise', devices);
+            return devices[0];
+            // return res[3].toJSON();
+        }).catch(err => {
+            console.log('error', err);
+        });
+};
+
+
+// console.log('support', supported);
+// console.log('device',device);
+
+device().then(deviceInfo => {
+    console.log('result: ', deviceInfo);
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+        console.log(`first`);
+        navigator.mediaDevices.getUserMedia({
+            video: true,
+            audio: {
+                groupId: {
+                    exact: deviceInfo.groupId
+                },
+                kind: {
+                    exact: "audioinput"
+                }
+            }
+        }).then(function (MediaStream) {
+            //video.src = window.URL.createObjectURL(stream);
+            videoElement.srcObject = MediaStream;
+            window.stream = MediaStream;
+            console.log(`stream: ${MediaStream}`);
+            videoElement.play();
+        });
+    } else if (navigator.getUserMedia) { // Standard
+        console.log(`standard`);
+        navigator.getUserMedia({
+            video: true,
+            audio: {
+                groupId: {
+                    exact: deviceInfo.groupId
+                },
+                kind: {
+                    exact: "audioinput"
+                }
+            }
+        }, function (stream) {
+            videoElement.src = stream;
+            console.log(`stream: ${stream}`);
+            videoElement.play();
+        }, errBack);
+    } else if (navigator.webkitGetUserMedia) { // WebKit-prefixed
+        console.log(`webkit`);
+        navigator.webkitGetUserMedia({
+            video: true,
+            audio: {
+                groupId: {
+                    exact: deviceInfo.groupId
+                    // 'b2b92d582ab6037118adba78cfbda558c0f072f410923b87d16e042f75963a8c'
+                },
+                kind: {
+                    exact: "audioinput"
+                }
+            }
+        }, function (stream) {
+            videoElement.src = window.webkitURL.createObjectURL(stream);
+            console.log(`stream: ${stream}`);
+            videoElement.play();
+        }, errBack);
+    } else if (navigator.mozGetUserMedia) { // Mozilla-prefixed
+        console.log(`moz`);
+        navigator.mozGetUserMedia({
+            video: true,
+            audio: {
+                groupId: {
+                    exact: deviceInfo.groupId
+                    // 'b2b92d582ab6037118adba78cfbda558c0f072f410923b87d16e042f75963a8c'
+                },
+                kind: {
+                    exact: "audioinput"
+                }
+            }
+        }, function (stream) {
+            videoElement.srcObject = stream;
+            console.log(`stream: ${stream}`);
+            videoElement.play();
+        }, errBack);
+    }
+});
+
+stopButton.addEventListener('click', () => {
+    // Stop all video streams.
+    videoElement.srcObject.getVideoTracks().forEach(track => track.stop());
+});
+
+//
+setInterval(() => {
+    context.drawImage(videoElement, 0, 0, 640, 480);
+    // console.log(`vid src: ${videoElement}`);
+    var src = canvasElement.toDataURL("image/webp");
+    socket.emit('newDraw', src);
+}, 1000 / fps);
+
+
+
+
 
 
 // socket.emit('stream',)
